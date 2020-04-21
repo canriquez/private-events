f8719e111274b9500c79cef8da3d79ef6ddc7bd9
Carlos Anriquez <canriquez@gmail.com>
2020-04-21 13:24:26 -0300

    User model and sessions controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
