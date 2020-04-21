ec6cd9be0ba151999e3bde5202be52d1d57f9ab3
Carlos Anriquez <canriquez@gmail.com>
2020-04-21 17:34:54 -0300

    updated event#create method to use build method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,13 @@
 class EventsController < ApplicationController
+
   def create
     @event = Event.new(event_params)
     @current_user = User.find_by(remember_me: cookies[:remember_me]);
-    @event.creator_id = @current_user.id if @current_user
-    if @current_user && @event.save
+    #@event.creator_id = @current_user.id if @current_user
+    if @current_user && @current_user.events.build(event_params).save
       flash[:notice] = "Event: #{@event.name} saved successfully"
       redirect_to root_path
     else
-
 
       render :new
     end
