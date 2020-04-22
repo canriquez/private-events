ec2a12b2f995e7b4971b7fad232d2ee963a60afc
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-22 15:52:41 +0000

    add scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,14 @@ class Event < ApplicationRecord
     has_many :guests, through: :event_invitations, source: :attendee
 
 
-    def self.future
-        self.all.includes(:creator).where("date >= ? ", Date.today);
-    end
+    # def self.future
+    #     self.all.includes(:creator).where("date >= ? ", Date.today);
+    # end
 
-    def self.past
-        self.all.includes(:creator).where("date <= ? ", Date.today);
-    end
+    # def self.past
+    #     self.all.includes(:creator).where("date <= ? ", Date.today);
+    # end
+
+    scope :future, -> {all.includes(:creator).where("date >= ? ", Date.today)}
+    scope :past, -> {all.includes(:creator).where("date <= ? ", Date.today)}
 end
