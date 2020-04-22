af71ad4b006fb7777de76171aed7df566f202e0a
Carlos Anriquez <canriquez@gmail.com>
2020-04-22 17:38:07 -0300

    finished initial capybara configuration and first draft signup test

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   resources :events, only: [:create, :new, :show, :index]
   root 'events#index'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:create, :show]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'signout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/integration/signup_test.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+# testing a signup
+
+RSpec.describe 'User creates a new registered user', type: :feature do
+    scenario 'they access the home page and click the signup button' do
+      visit signup_path
+  
+      fill_in 'name', with: 'exampleUser'
+      fill_in 'email', with: 'example@example.com'
+
+      click_button 'Sign up'
+  
+      expect(page).to have_css '.username', 'exampleUser'
+    end
+  end
+
