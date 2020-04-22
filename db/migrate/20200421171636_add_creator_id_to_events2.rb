0326c1642b3872548874598f87dadf9a7935741c
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-22 23:57:09 +0000

    fix migration issues

--- db/migrate/20200421171636_add_creator_id_to_events2.rb
+++ db/migrate/20200421171636_add_creator_id_to_events2.rb
@@ -1,5 +1,5 @@
 class AddCreatorIdToEvents2 < ActiveRecord::Migration[5.2]
   def change
-    add_foreign_key :events, :users, column: :creator_id, primary_key: "id"
+    # add_foreign_key :events, :users, column: :creator_id, primary_key: "id"
   end
 end
