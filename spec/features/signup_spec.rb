4868bf109f110a28994d6276d8b5ec9466e63596
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-22 20:58:40 +0000

    add integration test for sign up

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'User creates a new registered user', type: :feature do
     scenario 'they access the home page and click the signup button' do
       visit signup_path
   
-      fill_in 'name', with: 'exampleUser'
-      fill_in 'email', with: 'example@example.com'
+      fill_in 'user[name]', with: 'exampleUser'
+      fill_in 'user[email]', with: 'example@example.com'
 
       click_button 'Sign up'
   
-      expect(page).to have_css '.username', 'exampleUser'
+      expect(page).to have_content('exampleUser')
     end
   end
 
