f272f7e19e65587188b90924b643aadcb519028c
Carlos Anriquez <canriquez@gmail.com>
2020-04-23 19:14:08 -0300

    fixed bug on same_user? method preventing to show current user's event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def same_user?
-    return if current_user.id == params[:id]
+    return if current_user.id.to_s == params[:id]
 
     redirect_to root_path, alert: "You are not allowed to visit other users's profile!"
   end
