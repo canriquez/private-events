2d0f847543aeab9724102cafc4c0c953fac86d52
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-23 00:04:50 +0000

    fix migration issues

--- db/migrate/20200421211325_create_event_invitations.rb
+++ db/migrate/20200421211325_create_event_invitations.rb
@@ -2,7 +2,7 @@ class CreateEventInvitations < ActiveRecord::Migration[5.2]
   def change
     create_table :event_invitations do |t|
       t.references :event, foreign_key: true
-      t.references :attendee, foreign_key: true
+      # t.references :attendee, foreign_key: true
 
       t.timestamps
     end
